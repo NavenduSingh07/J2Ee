adff0046bcaa10379c3b13474a95e86fd3849372
Navendu Singh <54157440+NavenduSingh07@users.noreply.github.com>
2023-01-23 19:51:35 +0530

    Create jdbcDemo1.java

---
+++ jdbcDemo1.java
@@ -0,0 +1,41 @@
+package org.jspiders.jdbc_App;
+import java.sql.*;
+public class jdbcDemo {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement stmt = null;
+		String qry ="insert into btm.student value(2,'rohit',67.75)";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver class Loaded & Register");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=admin");
+			System.out.println("connection establish with database server");
+			stmt = con.createStatement();
+			System.out.println("Platform created");
+			stmt.executeUpdate(qry);
+			System.out.println("data inserted!!!");
+			}catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(stmt!=null)
+			{
+				try {
+					stmt.close();
+				}catch(SQLException e ) {e.printStackTrace();}
+			}
+			if(con!=null)
+			{
+				try {
+					con.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("closed all costly resources");
+		}
+
+	}
+
+}
